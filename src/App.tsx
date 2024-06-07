d3bb83dcbe3c4220e381e31109ae48c51a7927e6
Jayde Medder <jayde.medder@hotmail.com>
2024-06-07 16:22:58 +1200

    flex gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { AccordionDemo } from './client/components/AccordionDemo'
-import { CarouselDemo } from './client/components/CarouselDemo'
 import { ModeToggle } from './client/components/ModeToggle'
 import { ThemeProvider } from './client/components/ThemeProvider'
-import { Button } from './client/components/ui/button'
-import { Calendar } from './client/components/ui/calendar'
+import Gallery from './client/components/Gallery'
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <main className="p-24">
+      <main className="sm:p-12 p-4">
         <section className="pb-12 flex flex-row justify-between">
-          <h1 className="text-2xl font-bold">
-            Woah Shadcn + tailwind is sick!
-          </h1>
+          <h1 className="text-2xl font-bold">Lazy-Load Image Gallery</h1>
           <ModeToggle />
         </section>
-        <div className="px-24 flex flex-col justify-center">
-          <AccordionDemo />
-          <div className="flex gap-6 py-6 items-center">
-            <Button className="">Hello</Button>
-            <Button className="">Hi</Button>
-          </div>
-        </div>
-
-        <div className="flex justify-center">
-          <CarouselDemo />
-        </div>
-        <div>
-          <Calendar />
-        </div>
+        <Gallery />
       </main>
     </ThemeProvider>
   )
